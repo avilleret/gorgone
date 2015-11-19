fdaa149ea9603f426a48d5ae76b39b8a5c1f69f4
Antoine Villeret <antoine.villeret@gmail.com>
2015-11-19 22:48:24 +0100

    fix sound output to -1dB FS to avoid clipping

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo amixer cset numid=3 1
+amixer -c 0 -- sset PCM playback -1dB
 #pd -nogui -lib  Jamoma ${0%/*}/Pd/laser-driver.pd &
 
 LD_LIBRARY_PATH=/usr/local/jamoma/lib ${0%/*}/bin/gorgone -name ${HOSTNAME}
