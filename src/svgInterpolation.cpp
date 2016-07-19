e3b68e00acb57ee3b45a60a9c23e739b0c187b8c
stefane <retake7@gmail.com>
2016-07-19 14:17:52 +0200

    Revert "remove lineSize limitation"
    This reverts commit 4a59314132b5bb1aa41a2b2e5e4561d9886cc83c.

--- src/svgInterpolation.cpp
+++ src/svgInterpolation.cpp
@@ -13,6 +13,7 @@ void svgInterpolation::setup(){
   dir.listDir("formes_interpol/");
   dir.sort(); // in linux the file system doesn't return file lists ordered in alphabetical order
   ofLogNotice("svgInterpolation") << "found " << (int)dir.size() << " images to load.";
+  lineSize = 10000;
 
   for(int i = 0; i < (int)dir.size(); i++){
     // if ( i == 0 ) cout << "load : " << dir.getPath(i) << endl;
@@ -31,6 +32,7 @@ void svgInterpolation::setup(){
       vector<ofPolyline>& lines = const_cast<vector<ofPolyline>&>(p.getOutline());
 
       for(int k=0;k<(int)lines.size();k++){
+        lineSize = min(lineSize, (int) lines[k].size());
         ofPolyline line=lines[k];
         for(int n=0;n<line.size();n++){
 
@@ -97,6 +99,8 @@ void svgInterpolation::setup(){
     static_lines.push_back(myLine);
     }
   }
+
+  ofLogNotice("svgInterpolation") << "lineSize : " << lineSize << endl;
 }
 
 
@@ -131,11 +135,12 @@ bool svgInterpolation::multiInterpolation(){
   interpolatedLine.clear();
 
 /*
+  ofLogNotice("svgInterpolation") << "lineSize : " << lineSize << endl;
   ofLogNotice("svgInterpolation") << "nombre de formes chargées : " << lines.size() << endl;
   ofLogNotice("svgInterpolation") << "taille de coeff : " << coeff.size() << endl;
 */
 
-  for (int i = 0; i < shapeSize; i++){
+  for (int i = 0; i < lineSize; i++){
     ofVec3f pt;
     for (int j = 0; j < min(lines.size(), coeff.size()); j++){
       pt += coeff[j] * lines[j][i]; // [0] assumes that there is only one line on each ofPolyLine
