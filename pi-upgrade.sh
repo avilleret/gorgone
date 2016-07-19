6a7f4d470285160a30ab301f07abd51a1e187953
Antoine Villeret <antoine.villeret@gmail.com>
2016-07-19 12:12:56 +0200

    update pi in parallel

--- pi-upgrade.sh
+++ pi-upgrade.sh
@@ -1,3 +1,7 @@
-ssh pi@gorgone-1.local /home/pi/ofnode/gorgone/upgrade.sh
-ssh pi@gorgone-2.local /home/pi/ofnode/gorgone/upgrade.sh
-ssh pi@gorgone-3.local /home/pi/ofnode/gorgone/upgrade.sh
+# ssh pi@gorgone-1.local /home/pi/ofnode/gorgone/upgrade.sh
+# ssh pi@gorgone-2.local /home/pi/ofnode/gorgone/upgrade.sh
+# ssh pi@gorgone-3.local /home/pi/ofnode/gorgone/upgrade.sh
+
+osascript -e 'tell application "Terminal" to do script "ssh pi@gorgone-1.local /home/pi/ofnode/gorgone/upgrade.sh"'
+osascript -e 'tell application "Terminal" to do script "ssh pi@gorgone-2.local /home/pi/ofnode/gorgone/upgrade.sh"'
+osascript -e 'tell application "Terminal" to do script "ssh pi@gorgone-3.local /home/pi/ofnode/gorgone/upgrade.sh"'
