9c5d43551f780821f20cebcb1795b64bf4b5f89a
Antoine Villeret <antoine.villeret@gmail.com>
2016-10-03 18:22:21 +0200

    fix path error

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@ sudo amixer cset numid=3 1
 amixer -c 0 -- sset PCM playback -1dB
 #pd -nogui -lib  Jamoma ${0%/*}/Pd/laser-driver.pd &
 
-mkdir -p ${0%/*}/bin/data/images
-sshfs stefane@gorgone-master.local:/Users/stefane/gorgone/images/${HOSTNAME} ${0%/*}/bin/images
+sshfs stefane@gorgone-master.local:/Users/stefane/gorgone/images/${HOSTNAME} ${0%/*}/bin/data/images
 
 LD_LIBRARY_PATH=/usr/local/jamoma/lib ${0%/*}/bin/gorgone -name ${HOSTNAME} $@
